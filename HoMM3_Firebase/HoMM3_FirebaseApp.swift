b3e1bcd4a893ee4ea57ee5c77f51b6ac2eaf0801
m_mitin <maximus405@gmail.com>
2022-09-10 20:58:07 +0300

    adding some fixes to AppDelegate

--- HoMM3_Firebase/HoMM3_FirebaseApp.swift
+++ HoMM3_Firebase/HoMM3_FirebaseApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct HoMM3_FirebaseApp: App {
+    @StateObject var dataManager = DataManager()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(dataManager)
         }
     }
 }
