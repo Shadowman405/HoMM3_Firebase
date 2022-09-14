dd5d64ee7da5b83c2cd339a073a836f49bef41ea
m_mitin <maximus405@gmail.com>
2022-09-14 20:52:47 +0300

    add Artifact model

---
+++ HoMM3_Firebase/Model/Artifact.swift
@@ -0,0 +1,18 @@
+//
+//  Artifact.swift
+//  HoMM3_Firebase
+//
+//  Created by Maxim Mitin on 14.09.22.
+//
+
+import SwiftUI
+
+struct Artifact: Identifiable {
+    var id: Int
+    var name: String
+    var imageName: String
+    var slot: String
+    var effect: String
+    var description: String
+}
+
