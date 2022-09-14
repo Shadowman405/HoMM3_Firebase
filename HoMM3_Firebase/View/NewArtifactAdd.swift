0d8e3e7d687d0a38378a44ed5086dda2919884ce
m_mitin <maximus405@gmail.com>
2022-09-14 21:41:11 +0300

    configure AddArtView logic and layout

--- HoMM3_Firebase/View/NewArtifactAdd.swift
+++ HoMM3_Firebase/View/NewArtifactAdd.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct NewArtifactAdd: View {
+    @EnvironmentObject var dataManager: DataManager
+    @State private var id = ""
+    @State private var imageName = ""
+    @State private var name = ""
+    @State private var slot = ""
+    @State private var effect = ""
+    @State private var description = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("ID", text: $id)
+                .padding()
+            TextField("Image Name", text: $imageName)
+                .padding()
+            TextField("Name", text: $name)
+                .padding()
+            TextField("Slot", text: $slot)    .padding()
+            TextField("Effect", text: $effect)
+                .padding()
+            TextField("Description", text: $description)
+                .padding()
+            
+            Button {
+                dataManager.addArtifact(id: Int(id) ?? 0, imageName: imageName, name: name, slot: slot, effect: effect, description: description)
+            } label: {
+                Text("Save")
+            }
+
+        }
+        .padding()
     }
 }
 
 struct NewArtifactAdd_Previews: PreviewProvider {
     static var previews: some View {
         NewArtifactAdd()
+            .environmentObject(DataManager())
     }
 }
