900df5ce1d6652e4b09aa9c3802d7f93e6dd17b1
m_mitin <maximus405@gmail.com>
2022-09-27 21:50:43 +0300

    basic List config

--- HoMM3_Firebase/View/SkillsList.swift
+++ HoMM3_Firebase/View/SkillsList.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct SkillsList: View {
+    @EnvironmentObject var dataManager: DataManager
+    @State private var searchText = ""
+    @State private var showPopup = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(dataManager.skills, id: \.id) { skill in
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
 struct SkillsList_Previews: PreviewProvider {
     static var previews: some View {
         SkillsList()
+            .environmentObject(DataManager())
     }
 }
